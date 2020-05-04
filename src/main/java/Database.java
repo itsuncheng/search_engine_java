285b7b273fcaa6d070c2845aa435572f8ccfa577
jshian <jshian@connect.ust.hk>
2020-05-04 14:58:03 +0800

    add BdTypeEnum and change the design

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -6,79 +6,126 @@ import org.rocksdb.RocksIterator;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Map;
-import java.util.Vector;
+import java.util.*;
 
 
 public class Database
 {
-    public RocksDB db;
+    private RocksDB db;
     private Options options;
 
-    Database(String dbPath) throws RocksDBException
+    Database(String dbPath)
     {
-        this.options = new Options();
-        this.options.setCreateIfMissing(true);
-        this.db = RocksDB.open(options, dbPath);
+        try {
+            this.options = new Options();
+            this.options.setCreateIfMissing(true);
+            this.db = RocksDB.open(options, dbPath);
+        }catch (RocksDBException re){
+            re.printStackTrace();
+        }
+    }
+
+    public String IdBiConversion(String content) throws RocksDBException{
+        byte[] value = db.get("#".getBytes());
+        byte[] id = db.get(content.getBytes());
+        if (value == null){
+            //use # to represent the max word id/number of word so save the # to 1 in first word
+            //add word and word id bidirectional conversion in database
+            db.put("#".getBytes(),"1".getBytes());
+            db.put(content.getBytes(), "1".getBytes());
+            db.put("1".getBytes(), content.getBytes());
+            return "1";
+        }else if(id == null){//no saved before
+            // increase # by 1 and add word and word id conversion
+            int max = Integer.parseInt(new String(value));
+            byte[] newID = String.valueOf(max+1).getBytes();
+            db.put("#".getBytes(), newID);
+            db.put(content.getBytes(), newID);
+            db.put(newID, content.getBytes());
+            return String.valueOf(max+1);
+        }else return new String(id);
     }
 
     public void addBasicPageInfo(PageInfo pageInfo) throws RocksDBException
     {
         // information separated by ,
         String content = pageInfo.getBasicInfo();
-        db.put(pageInfo.getUrl().getBytes(), content.getBytes());
+        db.put(pageInfo.getPageID().getBytes(), content.getBytes());
     }
 
     public void addLinks(PageInfo pageInfo) throws RocksDBException
     {
-        // same type of link separated by , and child links and parent links are separated by space
-        String content = pageInfo.getChildLinkInfo() + " "+ pageInfo.getParentLinkInfo();
-        db.put(pageInfo.getUrl().getBytes(), content.getBytes());
+        Database page_ID_Bi = DbTypeEnum.getDbtypeEnum("Page_ID_Bi").getDatabase();
+        // change link to pageID
+        // same type of link separated by , and child links and parent links are separated by a space
+        String content = "";
+        for (String s:pageInfo.getChildLink()){
+            content += page_ID_Bi.IdBiConversion(s)+",";
+        }
+        content+=" ";
+
+        for (String s : pageInfo.getParentLink()) {
+            content += page_ID_Bi.IdBiConversion(s)+",";
+        }
+        db.put(pageInfo.getPageID().getBytes(), content.getBytes());
 
     }
 
     public void addKeywordFreq(PageInfo pageInfo) throws RocksDBException
     {
+        // change the keyword to wordID
         // keyword and its frequency are separated by ,
-        // different set of keyword are separated by space
-        String content = pageInfo.getKeywordFreqInfo();
-        db.put(pageInfo.getUrl().getBytes(), content.getBytes());
+        // different set of keyword are separated by a space
+        Database word_ID_Bi = DbTypeEnum.getDbtypeEnum("Word_ID_Bi").getDatabase();
+        String content = "";
+        Map<String, Integer> keywordFreq = pageInfo.getKeywordFreq();
+        for (String key : keywordFreq.keySet()) {
+            content += word_ID_Bi.IdBiConversion(key) +","+ keywordFreq.get(key) +" ";
+        }
+        db.put(pageInfo.getPageID().getBytes(), content.getBytes());
     }
 
     // record pageID and position y in order to perform phase search
-    public void addWord(String word, String pageID, int y) throws RocksDBException
+    public void addWord(String wordID, String pageID, int y) throws RocksDBException
     {
-        byte[] content = db.get(word.getBytes());
+        byte[] content = db.get(wordID.getBytes());
         if (content == null) {
             content = (pageID + "," + y).getBytes();
         } else {
             content = (new String(content) + " " + pageID + "," + y).getBytes();
         }
-        db.put(word.getBytes(), content);
+        db.put(wordID.getBytes(), content);
     }
 
     public void delEntry(String key) throws RocksDBException {
         db.remove(key.getBytes());
     }
 
-    public void printAll(RocksDB wordFile, RocksDB linkFile) throws RocksDBException
+    public static void printAll(RocksDB pageFile, RocksDB wordFile, RocksDB linkFile) throws RocksDBException
     {
 
         try {
+            Database word_ID_Bi = DbTypeEnum.getDbtypeEnum("Word_ID_Bi").getDatabase();
+            Database page_ID_Bi = DbTypeEnum.getDbtypeEnum("Page_ID_Bi").getDatabase();
             File file = new File("src/main/spider_result.txt");
             PrintWriter pw = new PrintWriter(file);
 
-            RocksIterator iter = db.newIterator();
+            RocksIterator iter = pageFile.newIterator();
             for(iter.seekToFirst(); iter.isValid(); iter.next()) {
                 String[] info = new String(iter.value()).split(",");
                 String output = "Page title: " + info[0] + "\nURL: " + new String(iter.key()) + "\nLast modification date: " + info[1]
                         + ", size of page: " + info[2] + "\n";
                 // words
-                output += new String(wordFile.get(iter.key())) + "\n";
+                String[] content = new String(wordFile.get(iter.key())).split(" ");
+                for (String s : content) {
+                    String[] key_value = s.split(",");
+                    output+= word_ID_Bi.IdBiConversion(key_value[0])+","+ key_value[1]+" ";
+                }
+                output += "\n";
                 // children links
                 String[] links = new String(linkFile.get(iter.key())).split(" ");
                 for (String s : links[0].split(",")) {
-                    output += s + "\n";
+                    output += page_ID_Bi.IdBiConversion(s) + "\n";
                 }
                 pw.write(output);
                 pw.write("-------------------------------------------------------------------------------------------" +
@@ -91,15 +138,15 @@ public class Database
 
     }
 
-    public boolean needUpdate(String url, String lastModDay){
-        RocksIterator iter = db.newIterator();
-        for(iter.seekToFirst(); iter.isValid(); iter.next()) {
-            if( url == (new String(iter.key())) ){
-                if((new String(iter.value())).contains(lastModDay))
-                    return true;
-            }
+    public boolean needUpdate(String URL, String lastModDay) throws RocksDBException{
+        Database page_ID_Bi = DbTypeEnum.getDbtypeEnum("Page_ID_Bi").getDatabase();
+        String pageID = page_ID_Bi.IdBiConversion(URL);
+        String content = new String(db.get(pageID.getBytes()));
+        if (content == null) return false;
+        else {
+            if (content.contains(lastModDay))return true;
+            return false;
         }
-        return false;
     }
 
     public RocksDB getDb(){
--- src/main/java/Indexer.java
+++ src/main/java/Indexer.java
@@ -6,11 +6,6 @@ import java.util.Map;
 import java.util.Vector;
 
 public class Indexer {
-    private Database titleIndex;
-    private Database bodyIndex;
-    private Database idToPageInfo;
-    private Database idToLinks;
-    private Database forwardIndex;
     private Crawler crawler;
     private StopStem stopStem;
     private Vector<String> visited;
@@ -19,46 +14,37 @@ public class Indexer {
     private Map<String, String> parentLinks;
 
     public Indexer(String rootURL, int numOfPage) {
-        try{
-            this.titleIndex = new Database("src/main/DB/titleDB");
-            this.bodyIndex = new Database("src/main/DB/bodyDB");
-            this.idToPageInfo = new Database("src/main/DB/pageDB");
-            this.idToLinks = new Database("src/main/DB/linkDB");
-            this.forwardIndex = new Database("src/main/DB/wordDB");
-            this.crawler = new Crawler(rootURL);
-            this.stopStem = new StopStem("src/main/stopwords.txt");
-            this.visited = new Vector<String>();
-            this.waitList = new Vector<String>();
-            this.pages = new Vector<PageInfo>();
-            this.parentLinks = new HashMap<String, String>();
-            //crawling
-            BFS(rootURL, numOfPage);
-            //add parent link
-            for (PageInfo page : pages) {
-                String[] link = parentLinks.get(page.getUrl()).split(",");
-                for (String l : link) {
-                    page.addParentLink(l);
-                }
+        this.crawler = new Crawler(rootURL);
+        this.stopStem = new StopStem("src/main/stopwords.txt");
+        this.visited = new Vector<String>();
+        this.waitList = new Vector<String>();
+        this.pages = new Vector<PageInfo>();
+        this.parentLinks = new HashMap<String, String>();
+        //crawling
+        BFS(rootURL, numOfPage);
+        //add parent link
+        for (PageInfo page : pages) {
+            String[] link = parentLinks.get(page.getUrl()).split(",");
+            for (String l : link) {
+                page.addParentLink(l);
             }
-
-        }catch (RocksDBException ex){
-            ex.printStackTrace();
         }
     }
 
     private void BFS(String rootURL, int numOfPage){
         try{
+            Database pageID_PageInfo = DbTypeEnum.getDbtypeEnum("PageID_PageInfo").getDatabase();
             // index first root url first
             String firstInWaitlist = rootURL;
-            if(idToPageInfo.needUpdate(firstInWaitlist, crawler.getLastModDay())){
-                idToPageInfo.delEntry(firstInWaitlist);
+            if(pageID_PageInfo.needUpdate(firstInWaitlist, crawler.getLastModDay())){
+                pageID_PageInfo.delEntry(firstInWaitlist);
             }
             visited.add(firstInWaitlist);
             Vector<String> childLink = crawler.extractLinks();
             waitList.addAll(childLink);
             int count = 1;
             recordParentLinks(firstInWaitlist, childLink);
-            indexing(firstInWaitlist,childLink);
+            indexing(rootURL,childLink);
             // index remaining url up to max page set by user
             while(!waitList.isEmpty() && count <= numOfPage){
                 // no more link
@@ -72,8 +58,8 @@ public class Indexer {
                 }
                 //update db
                 crawler = new Crawler(firstInWaitlist);
-                if(idToPageInfo.needUpdate(firstInWaitlist, crawler.getLastModDay())){
-                    idToPageInfo.delEntry(firstInWaitlist);
+                if(pageID_PageInfo.needUpdate(firstInWaitlist, crawler.getLastModDay())){
+                    pageID_PageInfo.delEntry(firstInWaitlist);
                 }
                 //successful extract information from a link
                 childLink = crawler.extractLinks();
@@ -94,18 +80,28 @@ public class Indexer {
 
     public void indexing(String URL, Vector<String> childLink){
         try{
+            Database titleInvertedFile = DbTypeEnum.getDbtypeEnum("TitleInvertedFile").getDatabase();
+            Database bodyInvertedFile = DbTypeEnum.getDbtypeEnum("BodyInvertedFile").getDatabase();
+            Database page_ID_Bi = DbTypeEnum.getDbtypeEnum("Page_ID_Bi").getDatabase();
+            Database pageID_PageInfo = DbTypeEnum.getDbtypeEnum("PageID_PageInfo").getDatabase();
+            Database pageID_Links = DbTypeEnum.getDbtypeEnum("PageID_Links").getDatabase();
+            Database forwardIndex = DbTypeEnum.getDbtypeEnum("ForwardIndex").getDatabase();
+
             // initialize page information data structure
             String title = crawler.getPageTitle();
-            PageInfo pageInfo = new PageInfo(title, URL, crawler.getLastModDay(), crawler.getSizeOfPage());
+            String pageID = page_ID_Bi.IdBiConversion(URL);
+            PageInfo pageInfo = new PageInfo(pageID, title, URL, crawler.getLastModDay(), crawler.getSizeOfPage());
+
             for (String cl : childLink) {
+                page_ID_Bi.IdBiConversion(cl);
                 pageInfo.addChildLink(cl);
             }
             // add title words to database
-            indexWord(URL, title, titleIndex, pageInfo);
+            indexWord(pageID, title, titleInvertedFile, pageInfo);
             // add body words to database
-            indexWord(URL, crawler.getPageBody(), bodyIndex, pageInfo);
-            idToPageInfo.addBasicPageInfo(pageInfo);
-            idToLinks.addLinks(pageInfo);
+            indexWord(pageID, crawler.getPageBody(), bodyInvertedFile, pageInfo);
+            pageID_PageInfo.addBasicPageInfo(pageInfo);
+            pageID_Links.addLinks(pageInfo);
             forwardIndex.addKeywordFreq(pageInfo);
             pages.add(pageInfo);
 
@@ -116,15 +112,17 @@ public class Indexer {
         }
     }
     // index word and add keyword to pageInfo
-    public void indexWord(String URL, String words, Database dbfile, PageInfo pageInfo) throws RocksDBException {
-    	String[] wordList = words.trim().split(" ");
+    public void indexWord(String pageID, String words, Database dbfile, PageInfo pageInfo) throws RocksDBException {
+        Database word_ID_Bi = DbTypeEnum.getDbtypeEnum("Word_ID_Bi").getDatabase();
+        String[] wordList = words.trim().split(" ");
         String result;
         for (int i = 0; i < wordList.length; i++) {
             if(!stopStem.isStopWord(wordList[i])){
                 result = stopStem.stem(wordList[i]);
                 if(result.length() > 0) {
-                    dbfile.addWord(wordList[i], URL, i+1);
-                    pageInfo.addKeyword(wordList[i]);
+                    String wordID = word_ID_Bi.IdBiConversion(wordList[i]);
+                    dbfile.addWord(wordID, pageID, i+1);
+                    pageInfo.addKeyword(wordList[i]);//
                 }
             }
         }
@@ -145,26 +143,14 @@ public class Indexer {
         }
     }
 
-    public Database getTitleIndex() {
-        return titleIndex;
-    }
-
-    public Database getBodyIndex() {
-        return bodyIndex;
-    }
-
-    public Database getIdToPageInfo() {
-        return idToPageInfo;
-    }
-
-    public Database getForwardIndex() {return forwardIndex;}
-
-    public Database getIdToLinks() {return idToLinks;}
-
     public static void main(String[] args) {
         try{
             Indexer in = new Indexer("http://www.cse.ust.hk", 30);
-            in.getIdToPageInfo().printAll(in.getForwardIndex().getDb(), in.getIdToLinks().getDb());
+            Database pageID_PageInfo = new Database("src/main/DB/PageID_PageInfo");
+            Database pageID_Links = new Database("src/main/DB/PageID_Links");
+            Database forwardIndex = new Database("src/main/DB/ForwardIndex");
+
+            Database.printAll(pageID_PageInfo.getDb(), pageID_Links.getDb(), forwardIndex.getDb());
         }catch (RocksDBException re){
             re.printStackTrace();
         }
